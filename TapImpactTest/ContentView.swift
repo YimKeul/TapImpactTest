65fe45202b171a51abb69d00c56b4463d233ef18
YimKeul <leesjun29@gmail.com>
2023-03-27 01:42:32 +0900

    feat : test code

--- TapImpactTest/ContentView.swift
+++ TapImpactTest/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let impact = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Button("보통 세기") {
+                // 버튼이 탭될 때마다 진동 피드백 실행
+                impact.impactOccurred()
+            }
+            .padding()
+            
+            Button("가장 세게") {
+                // 버튼이 탭될 때마다 진동 피드백 실행
+                impact.impactOccurred(intensity: 1.0)
+            }
+            .padding()
+            
+            Button("진동없음"){
+                
+            }.padding()
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
