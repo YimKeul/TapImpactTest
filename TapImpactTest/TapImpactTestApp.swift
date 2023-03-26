c7d969501fed06cb12119ef4cfcf817658298d08
YimKeul <leesjun29@gmail.com>
2023-03-27 01:27:28 +0900

    Initial Commit

---
+++ TapImpactTest/TapImpactTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapImpactTestApp.swift
+//  TapImpactTest
+//
+//  Created by yimkeul on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct TapImpactTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
